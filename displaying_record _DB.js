154c6c68c783143b6bcd9f81d2058587da10d131
Madeleine <46178706+madeleine68@users.noreply.github.com>
2021-06-06 19:34:52 -0400

    Add files via upload

---
+++ displaying_record _DB.js
@@ -0,0 +1,11 @@
+//Get and display numbers on all incidents v2
+var incGr = new GlideRecord("incident");
+incGr.addQuery("active", true);
+incGr.orderBy("number");
+incGr.setLimit(5);
+incGr.query();
+
+while (incGr.next()){
+  gs.info(incGr.number)
+}
+
